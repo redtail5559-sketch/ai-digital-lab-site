5b82a61802c01cab612843201341e8ad57dc6612
redtail5559-sketch <229195821+redtail5559-sketch@users.noreply.github.com>
2025-10-02 15:54:41 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    const isProd =
+      process.env.VERCEL_ENV === 'production' ||
+      process.env.NODE_ENV === 'production';
+
+    return isProd
+      ? [
+          {
+            source: '/(.*)',
+            headers: [
+              {
+                key: 'Strict-Transport-Security',
+                value: 'max-age=31536000; includeSubDomains; preload',
+              },
+            ],
+          },
+        ]
+      : [];
+  },
+};
+
 export default nextConfig;
